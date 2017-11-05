8b6aaa526ba9b630f25e29200e874b896823b819
EdgarFCrespo <fuentescrespoe@gmail.com>
2017-11-05 16:48:35 -0600

    Clase de prueba lista

--- Paint/src/PruebaPaint.java
+++ Paint/src/PruebaPaint.java
@@ -1,8 +1,13 @@
-
+import javax.swing.JFrame;
 public class PruebaPaint {
 
     public static void main(String[] args) {
         
+        VentanaD obj = new VentanaD();
+        
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.setSize(700,500);
+        obj.setVisible(true);
 	}//fin de main
 
 }//fin de la clase 
